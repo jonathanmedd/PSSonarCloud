f8e4301f6de38d9310a0b352722d9f2804652bc0
Jonathan Medd <jon_medd@hotmail.com>
2021-06-11 15:41:39 +0100

    Added Get-SonarCloudPermissionTemplate and Initialize-SonarCloudPermissionTemplate

---
+++ src/Functions/Public/Permissions/Initialize-SonarCloudPermissionTemplate.ps1
@@ -0,0 +1,74 @@
+﻿function Initialize-SonarCloudPermissionTemplate {
+    <#
+    .SYNOPSIS
+    Apply a SonarCloud Permission Template
+
+    .DESCRIPTION
+    Apply a SonarCloud Permission Template
+
+    .PARAMETER organization
+    SonarCloud organization
+
+    .PARAMETER projectKey
+    Project Key, e.g. GitLab repo id
+
+    .PARAMETER permissionTemplateName
+    Name of Permission Template to apply
+
+    .INPUTS
+    System.String.
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Initialize-SonarCloudPermissionTemplate -organization 'organization1' -projectKey '32959331' -permissionTemplateName 'PermissionTemplate A'
+#>
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+
+    Param (
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$organization,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$projectKey,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$permissionTemplateName
+    )
+
+    begin {
+
+        # --- Check for the presence of $Script:SonarCloudConnection
+        xCheckScriptSonarCloudConnection
+
+        $apiUrl = '/permissions/apply_template'
+    }
+
+    process {
+
+        try {
+
+            $queryParameters = @{
+
+                organization = $organization
+                projectKey = $projectKey
+                templateName = $permissionTemplateName
+            }
+
+            if ($PSCmdlet.ShouldProcess($projectKey)){
+
+                Invoke-SonarCloudRestMethod -Method POST -URI $apiUrl -queryParameters $queryParameters -Verbose:$VerbosePreference
+            }
+
+        }
+        catch [Exception] {
+
+            throw
+        }
+    }
+}
