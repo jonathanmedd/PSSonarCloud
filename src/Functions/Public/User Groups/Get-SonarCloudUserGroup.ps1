791993317698ec44d683c9580aae5e5afc585ead
Jonathan Medd <jon_medd@hotmail.com>
2021-06-11 16:44:10 +0100

    New User Group functions

---
+++ src/Functions/Public/User Groups/Get-SonarCloudUserGroup.ps1
@@ -0,0 +1,117 @@
+﻿function Get-SonarCloudUserGroup {
+    <#
+    .SYNOPSIS
+    Get SonarCloud UserGroups
+
+    .DESCRIPTION
+    Get SonarCloud UserGroups
+
+    .PARAMETER organization
+    SonarCloud Organization
+
+    .PARAMETER groupName
+    SonarCloud Group Name
+
+    .PARAMETER pageSize
+    Specify how many records to return
+
+    .INPUTS
+    System.String.
+    System.Int
+
+    .OUTPUTS
+    System.Management.Automation.PSObject
+
+    .EXAMPLE
+    Get-SonarCloudUserGroup -organization 'organization1'
+
+    .EXAMPLE
+    Get-SonarCloudUserGroup -organization 'organization1' -groupName 'UserGroup A'
+#>
+    [CmdletBinding(DefaultParameterSetName="Standard")][OutputType('System.Management.Automation.PSObject')]
+
+    Param (
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$organization,
+
+        [parameter(Mandatory=$false,ParameterSetName="ByName")]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$groupName,
+
+        [parameter(Mandatory=$false)]
+        [ValidateNotNullOrEmpty()]
+        [Int]$pageSize = 100
+    )
+
+    begin {
+
+        # --- Check for the presence of $Script:SonarCloudConnection
+        xCheckScriptSonarCloudConnection
+
+        $apiUrl = '/user_groups/search'
+        function CalculateOutput([PSCustomObject]$userGroup){
+
+            [PSCustomObject] @{
+
+                Name = $userGroup.name
+                Id = $userGroup.id
+                Description = $userGroup.description
+                MembersCount = $userGroup.membersCount
+                Default = $userGroup.default
+            }
+        }
+    }
+
+    process {
+
+        try {
+
+            switch ($PsCmdlet.ParameterSetName) {
+
+                # --- Get UserGroup by name
+                'ByName' {
+
+                    foreach ($name in $groupName){
+
+                        $queryParameters = @{
+
+                            organization = $organization
+                            q = $name
+                            ps  = $pageSize
+                        }
+
+                        $response = Invoke-SonarCloudRestMethod -Method GET -URI $apiUrl -queryParameters $queryParameters -Verbose:$VerbosePreference
+
+                        foreach ($userGroup in $response.groups){
+
+                            CalculateOutput $userGroup
+                        }
+                    }
+
+                    break
+                }
+                # --- No parameters passed so return all UserGroups
+                'Standard' {
+
+                    $queryParameters = @{
+
+                        organization = $organization
+                        ps  = $pageSize
+                    }
+                    $Response = Invoke-SonarCloudRestMethod -Method GET -URI $apiUrl -queryParameters $queryParameters -Verbose:$VerbosePreference
+
+                    foreach ($userGroup in $response.groups){
+
+                        CalculateOutput $userGroup
+                    }
+                }
+            }
+        }
+        catch [Exception] {
+
+            throw
+        }
+    }
+}
---
+++ src/Functions/Public/User Groups/Remove-SonarCloudUserGroupMember.ps1
@@ -0,0 +1,76 @@
+﻿function Remove-SonarCloudUserGroupMember {
+    <#
+    .SYNOPSIS
+    Remove a SonarCloud User from a Group
+
+    .DESCRIPTION
+    Remove a SonarCloud User from a Group
+
+    .PARAMETER organization
+    SonarCloud Organization
+
+    .PARAMETER groupName
+    SonarCloud Group Name
+
+    .PARAMETER userLogin
+    SonarCloud User Login, e.g. testuser1@gitlab
+
+    .INPUTS
+    System.String.
+
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Remove-SonarCloudUserGroupMember -organization 'organization1' -groupName 'Group A' -userLogin 'testuser1@gitlab','testuser2@gitlab'
+#>
+    [CmdletBinding(SupportsShouldProcess,ConfirmImpact="High")]
+
+    Param (
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$organization,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]$groupName,
+
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String[]]$userLogin
+    )
+
+    begin {
+
+        # --- Check for the presence of $Script:SonarCloudConnection
+        xCheckScriptSonarCloudConnection
+
+        $apiUrl = '/user_groups/remove_user'
+    }
+
+    process {
+
+        try {
+
+            foreach ($user in $userLogin){
+
+                $queryParameters = @{
+
+                    organization = $organization
+                    name = $groupName
+                    login = $user
+                }
+
+                if ($PSCmdlet.ShouldProcess($groupName)){
+
+                    Invoke-SonarCloudRestMethod -Method POST -URI $apiUrl -queryParameters $queryParameters -Verbose:$VerbosePreference
+                }
+            }
+        }
+        catch [Exception] {
+
+            throw
+        }
+    }
+}
